eaa91ad159b303926d14eb7584f4b3101b5399fb
Benjamin Mclean <Benjamin Mclean>
2016-06-17 17:36:17 -0700

    add templates and servies

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','coachChip.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,5 +20,19 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-  });
-})
+  })
+  .config(function($stateProvider, $urlRouterProvider){
+    $stateProvider
+
+    .state('card.item',{
+      url:'/card',
+      views: {
+        'card-item':{
+          templateUrl: 'templates/mainscreen.html',
+          controller: 'cardCtrl'
+        }
+      }
+    });
+
+  })
+});
